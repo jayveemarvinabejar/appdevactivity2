9b5f2ecbeec40412396d853e3693eb802259f6b0
KenLauxzs <kenlauxzs@gmail.com>
2022-03-26 15:39:33 +0800

    Activity2

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contactlist = [
+    {id: 1, name: 'Ken Bryan Dipa', email: 'Kenlauxzs@gmail.com'  ,number: '09123124216'},
+    {id: 2, name: 'Kenlauxzs', email: 'Kenlauxzs@gmail.com'  ,number: '09164345316'},
+    {id: 3, name: 'Test123', email: 'Test123@gmail.com'  ,number: '09162132354'},
+   
+  ]
+
+
+
+  constructor() {
+  }
+
 
 }
+
+
+
+
