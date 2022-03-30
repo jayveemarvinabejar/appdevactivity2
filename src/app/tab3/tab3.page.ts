b69c53bd0208aa13d9a01d980788fad8e57aed52
KenLauxzs <kenlauxzs@gmail.com>
2022-03-30 11:08:32 +0800

    update for activit 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+
 
 
 
@@ -10,7 +12,7 @@ import { Component} from '@angular/core';
 })
 export class Tab3Page {
 
-  contactlist = [
+  contactlist  = [
     {id: 1, name: 'Ken Bryan Dipa', email: 'Kenlauxzs@gmail.com'  ,number: '09123124216'},
     {id: 2, name: 'Kenlauxzs', email: 'Kenlauxzs@gmail.com'  ,number: '09164345316'},
     {id: 3, name: 'Test123', email: 'Test123@gmail.com'  ,number: '09162132354'},
@@ -19,7 +21,112 @@ export class Tab3Page {
 
 
 
-  constructor() {
+
+  constructor(public alertController: AlertController, public toastController: ToastController ) {
+  }
+  async  confirmation(index: number) {
+    const alert = await this.alertController.create({
+      header: 'Are You Sure You Want To Delete?',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+
+              this.contactlist.splice(index, 1);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', 
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ],
+    });
+
+    await alert.present();
+  }
+  async addUser() {
+    let prompt = await this.alertController.create({
+      header: 'Add Contact',
+      message: "",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+        },{
+          name: 'email',
+          placeholder: '@gmail  <optional>',
+        },
+        {
+          name: 'number',
+          placeholder: 'Phone number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            console.log('Saved clicked');
+            var nameLetter = /^[A-Za-z]+$/;
+
+            if((data.name != null) && (data.number.length > 0)){
+              if(data.name.length > 15){
+              this.showErrorToast('<ion-text color="danger"><b>Name should be greater than 15 letters</b></ion-text>');
+              return false;
+              }
+              else if(data.number.length != 11){
+              this.showErrorToast('<ion-text color="danger"><b>Number should be 11 numbers only</b></ion-text>');
+              return false;
+              }
+              else if(!data.name.match(nameLetter)){
+              this.showErrorToast('<ion-text color="danger"><b>Name should be aphabet only</b></ion-text>');
+              return false;
+              }
+              else{
+                if(data.email.length == 0){
+                  data.email = "none"
+                }
+              this.contactlist.push({
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                number: data.number
+              });
+              this.showErrorToast('<ion-text color="danger"><b>Added</b></ion-text>');
+            }
+            }
+            else{
+              this.showErrorToast('<ion-text color="danger"><b>Pls fill in the blanks</b></ion-text>');
+              return false;
+            }
+          
+        
+          }
+        }
+      ]
+    });
+    await prompt.present();
+
+  }
+  async showErrorToast(data: any) {
+    let toast = this.toastController.create({
+      message: data,
+      duration: 3000,
+      position: 'top'
+    });
+
+   
+
+    (await toast).present();
   }
 
 
